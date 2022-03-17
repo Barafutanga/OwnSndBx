a0552d3adf5128d541b9c8f3da6b1ac79594c4c1
Otaviano <89816185+Barafutanga@users.noreply.github.com>
2022-03-17 10:34:59 -0300

    Math Game
    Tiny code with a calculator game.
    FEATURES:
    - Up to 2 operations (+ and *)
    - Calculate your score
    - Use random generated numbers

---
+++ DartMath.java
@@ -0,0 +1,37 @@
+package master;
+import java.util.Scanner;
+
+public class DartMath {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("WELCOME TO MATHTEST! LET'S CHECK YOUR MOM.. I MEAN, CALCULATING CAPACITY");
+
+        int x, y, answer, result, calc, score = -1;
+
+        do {
+        x = (int) Math.floor (Math.random() * 99);
+        y = (int) Math.floor (Math.random() * 99);
+        calc = (int) Math.floor (Math.random() * 2); 
+
+        if (calc == 0) {
+            result = x + y;
+        } else {
+            result = x * y;
+        }
+        
+        if (calc == 0) {
+        System.out.printf("%d + %d = ", x, y);
+        } else {
+            System.out.printf("%d * %d = ", x, y);
+        }
+
+        answer = sc.nextInt();
+        score++;
+        } while (result == answer);
+
+        System.out.printf("GAME OVER\nSCORE: %d", score);
+
+        sc.close();
+    }    
+}
